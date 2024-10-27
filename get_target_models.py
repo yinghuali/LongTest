e0f29a256a08270e7ceb852297239d1a53f178b0
yinghuali <yinghua.li@uni.lu>
2024-10-27 17:23:01 +0100

    update

--- get_target_models.py
+++ get_target_models.py
@@ -13,8 +13,8 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("--path_file_embedding_X", type=str)
 ap.add_argument("--path_file_embedding_y", type=str)
-ap.add_argument("--path_save_model", type=int)
-ap.add_argument("--model_name", type=int)
+ap.add_argument("--path_save_model", type=str)
+ap.add_argument("--model_name", type=str)
 args = ap.parse_args()
 
 
@@ -28,9 +28,9 @@ model_name = args.model_name
 # path_save_model = './target_models/MiniLM-L6-v2-rf.pkl'
 # model_name = 'rf'
 
-# python get_target_models.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_embedding_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_save_model './target_models/MiniLM-L6-v2-rf.pkl' --model_name 'rf'
-# python get_target_models.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_embedding_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_save_model './target_models/MiniLM-L6-v2-lr.pkl' --model_name 'lr'
-# python get_target_models.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_embedding_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_save_model './target_models/MiniLM-L6-v2-xgb.pkl' --model_name 'xgb'
+# python get_target_models.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_embedding_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_save_model './target_models/MiniLM-L6-v2-rf.model' --model_name 'rf'
+# python get_target_models.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_embedding_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_save_model './target_models/MiniLM-L6-v2-lr.model' --model_name 'lr'
+# python get_target_models.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_embedding_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_save_model './target_models/MiniLM-L6-v2-xgb.model' --model_name 'xgb'
 
 
 def select_model(model_name):
@@ -54,11 +54,13 @@ def main():
 
     model = select_model(model_name)
     model.fit(embedding_train_vec, y_train)
+    joblib.dump(model, path_save_model)
 
     y_pre_test = model.predict(embedding_test_vec)
     y_pre_train = model.predict(embedding_train_vec)
     acc_train = accuracy_score(y_train, y_pre_train)
     acc_test = accuracy_score(y_test, y_pre_test)
+
     print('acc_train:', acc_train)
     print('acc_test:', acc_test)
 
