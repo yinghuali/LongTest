63bd65fbdc222ffe386a599af3abc7af0d5c5cff
yinghuali <yinghua.li@uni.lu>
2024-10-29 15:25:19 +0100

    update

--- run_largeFileTest.sh
+++ run_largeFileTest.sh
@@ -8,4 +8,6 @@
 #SBATCH --time=1-23:00:00
 
 python largeFileTest.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_chunk_embedding_X './data/embedding_data/EURLEX57K_chunk_X_10.pkl' --path_target_model './target_models/MiniLM-L6-v2-rf.model' --path_save_res './result/MiniLM-L6-v2-rf_10.json'
+python largeFileTest.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_chunk_embedding_X './data/embedding_data/EURLEX57K_chunk_X_10.pkl' --path_target_model './target_models/MiniLM-L6-v2-dt.model' --path_save_res './result/MiniLM-L6-v2-dt_10.json'
+python largeFileTest.py --path_file_embedding_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_file_y './data/embedding_data/EURLEX57K_file_y.pkl' --path_chunk_embedding_X './data/embedding_data/EURLEX57K_chunk_X_10.pkl' --path_target_model './target_models/MiniLM-L6-v2-lr.model' --path_save_res './result/MiniLM-L6-v2-lr_10.json'
 
