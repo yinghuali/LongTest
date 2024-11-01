5363f80290f4221976d4dd5c3aa0116370b9ee8f
yinghuali <yinghua.li@uni.lu>
2024-11-01 16:12:41 +0100

    update

--- run_chunk_embedding.sh
+++ run_chunk_embedding.sh
@@ -8,19 +8,19 @@
 #SBATCH -p batch
 #SBATCH --mem 10G
 
-python get_chunk_embedding.py --num_chunks 5 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/EURLEX57K_chunk_X_5.pkl'
-python get_chunk_embedding.py --num_chunks 10 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/EURLEX57K_chunk_X_10.pkl'
-python get_chunk_embedding.py --num_chunks 15 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/EURLEX57K_chunk_X_15.pkl'
-python get_chunk_embedding.py --num_chunks 20 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/EURLEX57K_chunk_X_20.pkl'
+python get_chunk_embedding.py --num_chunks 5 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/EURLEX57K_chunk_X_5.pkl'
+python get_chunk_embedding.py --num_chunks 10 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/EURLEX57K_chunk_X_10.pkl'
+python get_chunk_embedding.py --num_chunks 15 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/EURLEX57K_chunk_X_15.pkl'
+python get_chunk_embedding.py --num_chunks 20 --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/EURLEX57K_chunk_X_20.pkl'
 
 
-python get_chunk_embedding.py --num_chunks 5 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/FakeNews_chunk_X_5.pkl'
-python get_chunk_embedding.py --num_chunks 10 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/FakeNews_chunk_X_10.pkl'
-python get_chunk_embedding.py --num_chunks 15 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/FakeNews_chunk_X_15.pkl'
-python get_chunk_embedding.py --num_chunks 20 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/FakeNews_chunk_X_20.pkl'
+python get_chunk_embedding.py --num_chunks 5 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/FakeNews_chunk_X_5.pkl'
+python get_chunk_embedding.py --num_chunks 10 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/FakeNews_chunk_X_10.pkl'
+python get_chunk_embedding.py --num_chunks 15 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/FakeNews_chunk_X_15.pkl'
+python get_chunk_embedding.py --num_chunks 20 --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/FakeNews_chunk_X_20.pkl'
 
 
-python get_chunk_embedding.py --num_chunks 5 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/CancerDoc_chunk_X_5.pkl'
-python get_chunk_embedding.py --num_chunks 10 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/CancerDoc_chunk_X_10.pkl'
-python get_chunk_embedding.py --num_chunks 15 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/CancerDoc_chunk_X_15.pkl'
-python get_chunk_embedding.py --num_chunks 20 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/CancerDoc_chunk_X_20.pkl'
+python get_chunk_embedding.py --num_chunks 5 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/CancerDoc_chunk_X_5.pkl'
+python get_chunk_embedding.py --num_chunks 10 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/CancerDoc_chunk_X_10.pkl'
+python get_chunk_embedding.py --num_chunks 15 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/CancerDoc_chunk_X_15.pkl'
+python get_chunk_embedding.py --num_chunks 20 --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/CancerDoc_chunk_X_20.pkl'
--- run_file_embedding.sh
+++ run_file_embedding.sh
@@ -8,8 +8,6 @@
 #SBATCH -p batch
 #SBATCH --mem 10G
 
-# python get_file_embedding.py --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/EURLEX57K_file_X.pkl' --path_save_y './data/embedding_data/EURLEX57K_file_y.pkl'
-
-# python get_file_embedding.py --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/FakeNews_file_X.pkl' --path_save_y './data/embedding_data/FakeNews_file_y.pkl'
-
-python get_file_embedding.py --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/CancerDoc_file_X.pkl' --path_save_y './data/embedding_data/CancerDoc_file_y.pkl'
+python get_file_embedding.py --path_data './data/EURLEX57K/df_all_EURLEX57K.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/EURLEX57K_file_X.pkl' --path_save_y './data/embedding_data/all-MiniLM-L6-v2/EURLEX57K_file_y.pkl'
+python get_file_embedding.py --path_data './data/FakeNews/df_all_FakeNews.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/FakeNews_file_X.pkl' --path_save_y './data/embedding_data/all-MiniLM-L6-v2/FakeNews_file_y.pkl'
+python get_file_embedding.py --path_data './data/CancerDoc/df_all_CancerDoc.csv' --path_save_X './data/embedding_data/all-MiniLM-L6-v2/CancerDoc_file_X.pkl' --path_save_y './data/embedding_data/all-MiniLM-L6-v2/CancerDoc_file_y.pkl'
